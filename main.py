c85aaf8441628a26cf6b85e442b1bf4582bc08b6
Joshua Hecker <jhecker2001@gmail.com>
2023-11-30 11:00:35 -0600

    Added functionality to create a playlist for the user and add the songs to the playlist.

--- main.py
+++ main.py
@@ -28,8 +28,13 @@ with open("client_data.txt") as file:
 oauth = SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri="http://example.com",
                      scope="playlist-modify-private")
 
-with open(".cache") as file:
-    user_token = eval(file.readline())["access_token"]
+try:
+    with open(".cache") as file:
+        user_token = eval(file.readline())["access_token"]
+except spotipy.SpotifyException:
+    oauth.get_cached_token()
+    with open(".cache") as file:
+        user_token = eval(file.readline())["access_token"]
 
 sp = spotipy.Spotify(user_token)
 user_id = sp.current_user()["id"]
@@ -45,9 +50,18 @@ for song in song_list:
     else:
         results_list.append(result)
 
-with open(file="results.txt", mode="w") as file:
-    pp = pprint.PrettyPrinter(stream=file)
-    pp.pprint(results_list)
+# with open(file="results.txt", mode="w") as file:
+#     pp = pprint.PrettyPrinter(stream=file)
+#     pp.pprint(results_list)
+
+playlist_name = f"{date} Billboard 100"
+playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=False)
+playlist_id = playlist["id"]
+
+track_ids = [song["tracks"]["items"][0]["uri"] for song in results_list]
+
+sp.playlist_add_items(playlist_id=playlist_id, items=track_ids)
+print(f"Congratulations! You now have a brand new Spotify playlist called '{playlist_name}'! Go check it out!")
 
 # print(song_list)
 # print(artist_list)
